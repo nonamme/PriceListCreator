e3d0a3988beba42d7514ef994c3a10179a47fab5
nonamme <klewandowski90@gmail.com>
2019-08-08 22:28:54 +0100

    Add route for editing warehouse

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get 'price_list/index', :to => "price_lists#index"
   get 'price_list/add_warehouse', :to => "price_lists#addWarehouse"
+  get 'price_list/edit_warehouse', :to => "price_lists#editWarehouse" # edit warehouse require passing warehouse id to edit
 end
