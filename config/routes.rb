69b2d6b1f4d9c02ce9e4ca429c0586a6c5443643
nonamme <klewandowski90@gmail.com>
2019-08-15 22:20:24 +0100

    Add root and destroy warehouse routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  root "price_lists#index"
   get 'price_list/index', :to => "price_lists#index"
   get 'price_list/add_warehouse', :to => "price_lists#addWarehouse"
   get 'price_list/edit_warehouse', :to => "price_lists#editWarehouse" # edit warehouse require passing warehouse id to edit
+
+  delete 'warehouse/destroy/:id', :to => "warehouses#delete", as: :warehouse_destroy
 end
