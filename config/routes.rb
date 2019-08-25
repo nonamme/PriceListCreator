c09952b5fecaff166a119d655a3e62446bcc9294
nonamme <klewandowski90@gmail.com>
2019-08-26 00:06:40 +0100

    Update name for paths methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root "price_lists#index"
   get 'price_list/index', :to => "price_lists#index"
-  get 'price_list/add_warehouse', :to => "price_lists#addWarehouse"
-  get 'price_list/edit_warehouse', :to => "price_lists#editWarehouse" # edit warehouse require passing warehouse id to edit
+  get 'price_list/add_warehouse', :to => "price_lists#addWarehouse", as: :add_warehouse
+  post 'price_list/create_warehouse', :to => "price_lists#createWarehouse", as: :create_warehouse
+  get 'price_list/edit_warehouse/:id', :to => "price_lists#editWarehouse", as: :edit_warehouse# edit warehouse require passing warehouse id to edit
 
   delete 'warehouse/destroy/:id', :to => "warehouses#delete", as: :warehouse_destroy
   get 'warehouse/search', :to => "warehouses#search", as: :warehouse_search
