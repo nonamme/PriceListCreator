e3ac42ba39d1a6e919d873724685095e73653fae
nonamme <klewandowski90@gmail.com>
2019-08-26 00:03:57 +0100

    Adding warehouses

--- app/controllers/price_lists_controller.rb
+++ app/controllers/price_lists_controller.rb
@@ -4,11 +4,49 @@ class PriceListsController < ApplicationController
   end
 
   def addWarehouse
-    @name = :warehouse
     render 'add_warehouse'
   end
 
+  def createWarehouse
+    w = Warehouse.new(
+        warehouse_name: params[:warehouse_name],
+        supplier: params[:supplier_name],
+        product: params[:product_name])
+    wd = WarehouseDetail.create(
+        gross_price: params[:gross_price],
+        sum_24_pal: params[:transport_24_palettes],
+        price_pellete_per_tone: params[:pellet_price_per_tone],
+        bag_price: params[:bag_price],
+        palette_price: params[:palette_charge],
+        order_price: params[:order_charge],
+        warehouse: w
+    )
+
+    w.save
+
+    params[:areas].each do |area|
+      a = Area.create(
+          :number => area[0][-1],
+          :post_codes => area[1][:postcode],
+          warehouse: w)
+      area[1].each do |k, v|
+        p k
+        # p v
+        pl = PriceList.create(
+            :number_of_pallets => k,
+            :net_rabate => v[:rabate],
+            :net_logistic => v[:logistic],
+            :area => a
+        ) unless k.eql? "postcode"
+      end
+    end
+
+    redirect_to root_path
+  end
+
+
   def editWarehouse
+
     render 'edit_warehouse'
   end
 end
