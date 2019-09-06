aa453c0c9cb0ac2a1c1bb9a5b204ba8b36478a9a
nonamme <klewandowski90@gmail.com>
2019-09-06 23:32:42 +0100

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'price_list/add_warehouse', :to => "price_lists#addWarehouse", as: :add_warehouse
   post 'price_list/create_warehouse', :to => "price_lists#createWarehouse", as: :create_warehouse
   get 'price_list/edit_warehouse/:id', :to => "price_lists#editWarehouse", as: :edit_warehouse# edit warehouse require passing warehouse id to edit
+  put 'price_list/update_warehouse/:id', :to => "price_lists#updateWarehouse", as: :update_warehouse
 
   delete 'warehouse/destroy/:id', :to => "warehouses#delete", as: :warehouse_destroy
   get 'warehouse/search', :to => "warehouses#search", as: :warehouse_search
