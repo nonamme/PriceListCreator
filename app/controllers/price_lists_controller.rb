9caa5ba7bdcdc78972a545f5689674ff866ad963
nonamme <klewandowski90@gmail.com>
2019-09-06 23:30:12 +0100

    Edit for warehouses

--- app/controllers/price_lists_controller.rb
+++ app/controllers/price_lists_controller.rb
@@ -30,8 +30,6 @@ class PriceListsController < ApplicationController
           :post_codes => area[1][:postcode],
           warehouse: w)
       area[1].each do |k, v|
-        p k
-        # p v
         pl = PriceList.create(
             :number_of_pallets => k,
             :net_rabate => v[:rabate],
@@ -46,7 +44,46 @@ class PriceListsController < ApplicationController
 
 
   def editWarehouse
-
+    @warehouse = Warehouse.find(params[:id])
     render 'edit_warehouse'
   end
+
+  def updateWarehouse
+    w = Warehouse.find(params[:id])
+    w.update warehouse_params
+
+    wd = WarehouseDetail.where(warehouse: w).first
+    wd.update warehouse_details_params
+
+
+
+    area_params.each do |area|
+      a = Area.where(warehouse: w).where(id: area.first.to_i).first
+      a.update post_codes: area.second[:post_codes]
+
+      a.price_list.each_with_index do |price_list, index|
+        b = PriceList.find(price_list.id)
+        b.update net_rabate: area.second["#{index + 1}"][:net_rabate], net_logistic: area.second["#{index + 1}"][:net_logistic]
+      end
+    end
+
+    redirect_to root_path
+  end
+
+  private
+    def warehouse_params
+      params.require(:warehouse).permit(:supplier, :product, :warehouse_name)
+    end
+
+    def warehouse_details_params
+      params.require(:warehouse_details).permit(:gross_price, :sum_24_pal, :price_pellete_per_tone, :bag_price, :palette_price, :order_price)
+    end
+
+    def area_params
+      params.require(:areas).permit!
+    end
+
+    # def price_list_params
+    #   params.require(:price_list).permit!
+    # end
 end
